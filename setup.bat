d168e9a35e53ca75609a846ac392a355562015a0
daicham <daicham@gmail.com>
2012-04-12 09:30:56 +0900

    add setup.bat for Windows
    require Windows2003 Resouce Kit Tools.
    * http://www.microsoft.com/download/en/details.aspx?DisplayLang=en%2cen&id=17657

---
+++ setup.bat
@@ -0,0 +1,6 @@
+fsutil hardlink create ~/.gitconfig ~/dotfiles/.gitconfig
+fsutil hardlink create ~/_vimrc ~/dotfiles/_vimrc
+fsutil hardlink create ~/_gvimrc ~/dotfiles/_gvimrc
+fsutil hardlink create ~/.bashrc ~/dotfiles/.bashrc
+linkd ~/.vim ~/dotfiles/vimfiles
+
