02efdce29105f98f956c84635cc63a09328f7f67
daicham <daicham@gmail.com>
2012-07-11 12:48:35 +0900

    Change home dir to using %USERPROFILE%

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
-fsutil hardlink create ~/.gitconfig ~/dotfiles/.gitconfig
-fsutil hardlink create ~/_vimrc ~/dotfiles/_vimrc
-fsutil hardlink create ~/_gvimrc ~/dotfiles/_gvimrc
-fsutil hardlink create ~/.bashrc ~/dotfiles/.bashrc
-linkd ~/.vim ~/dotfiles/vimfiles
+fsutil hardlink create "%USERPROFILE%\.gitconfig" "%USERPROFILE%\dotfiles\.gitconfig"
+fsutil hardlink create "%USERPROFILE%\_vimrc" "%USERPROFILE%\dotfiles\_vimrc"
+fsutil hardlink create "%USERPROFILE%\_gvimrc" "%USERPROFILE%\dotfiles\_gvimrc"
+fsutil hardlink create "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfiles\.bashrc"
+linkd "%USERPROFILE%/.vim" "%USERPROFILE%/dotfiles/vimfiles"
 
